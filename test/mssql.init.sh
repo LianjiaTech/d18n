da942700ec088a511511755ae85c8eb7f0c99190
Leon Zhang <martianzhang@hotmail.com>
2022-04-18 12:05:39 +0800

    mssql add sakila sample database

--- test/mssql.init.sh
+++ test/mssql.init.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+cd /docker-entrypoint-initdb.d/ && tar zxf sakila.mssql.sql.gz
 # Wait to be sure that SQL Server came up
 for i in {1..50};
 do
     /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'yourStrong(!)Password' -i /docker-entrypoint-initdb.d/mssql.init.sql >/dev/null 2>&1
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'yourStrong(!)Password' -i /docker-entrypoint-initdb.d/sakila.mssql.sql >/dev/null 2>&1
     if [ $? -eq 0 ]
     then
 	echo ""
-        echo "mssql.init.sql completed"
+        echo "sql init completed"
         break
     else
         echo -n "."
